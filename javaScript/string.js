c21184c97f3d4e4c2175ec38311994355928767e
Sakib-203-15-3883 <sakibul15.3883@gmail.com>
2024-01-10 19:05:32 +0600

    ..

--- javaScript/string.js
+++ javaScript/string.js
@@ -1,11 +1,11 @@
 //To find the length of a string, use the built-in length property
 let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let newText = text.length;
-console. log(  newText );
+console.log(newText);
 
-//Escape Character
-let text2 = "We are the so-called \"Vikings\" from the north.";
-console. log(  text2 );
+//Escape Characterd
+let text2 = 'We are the so-called "Vikings" from the north.';
+console.log(text2);
 
 //You can also break up a code line within a text string with a single backslash:
 
@@ -14,14 +14,12 @@ console. log(  text2 );
 
 //Note the difference between (x==y) and (x===y).\
 
-let num1="10";
-let num2=10;
+let num1 = "10";
+let num2 = 10;
 
-console. log(num1==num2);   // result true . cheack only value 
-console. log(num1===num2);  // result false . cheack value and data type.
+console.log(num1 == num2); // result true . cheack only value
+console.log(num1 === num2); // result false . cheack value and data type.
 
 //Comparing two JavaScript objects always returns false.
 let x = new String("John");
 let y = new String("John");
-
-
